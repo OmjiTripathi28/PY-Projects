72053ea9ca10874fc4b49027ef80d228edbce0ed
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-01-20 17:44:51 +0530

    added features

--- EASY/Dice Rolling Game (Pig Game).py
+++ EASY/Dice Rolling Game (Pig Game).py
@@ -10,48 +10,46 @@ while True:
     print("\nYOUR TURN")
 
     while True:
-        choice = input("ROLL or PASS: ").strip().upper()
-
+        choice = input("ROLL or PASS : ").strip().upper()
         if choice == "ROLL":
-            dice = random.randint(1, 6)
+            dice = random.randint(1,6)
             print(f"You rolled: {dice}")
 
-            if dice == 1:
+            if dice == 1 :
                 user_turn_score = 0
-                print("Turn lost! No points added.")
+                print("YOU LOST THE TURN! No points added.")
                 break
+
             else:
                 user_turn_score += dice
-                print(f"Turn score: {user_turn_score}")
-
+                print(f"TURN SCORE : {user_turn_score}")
         elif choice == "PASS":
             total_user_score += user_turn_score
-            print(f"Total score: {total_user_score}")
+            print(f"YOUR TOTAL SCORE : {total_user_score}")
             break
         else:
-            print("Invalid choice.")
+            print("INVALID INPUT")
 
     if total_user_score >= WIN_SCORE:
-        print("YOU WIN!")
+        print("YOU WIN!!")
         break
-
     
     comp_turn_score = 0
     print("\nCOMPUTER'S TURN")
 
     while comp_turn_score < 20:
-        dice = random.randint(1, 6)
+        dice = random.randint(1,6)
         print(f"Computer rolled: {dice}")
-
-        if dice == 1:
+        if dice == 1 :
             comp_turn_score = 0
-            print("Computer lost its turn!")
+            print("COMPUTER LOST THE TURN!")
             break
         else:
             comp_turn_score += dice
+            print(f"COMPUTER TURN SCORE : {comp_turn_score}")
 
     total_comp_score += comp_turn_score
-    print(f"Computer total score: {total_comp_score}")
+    print(f"COMPUTER TOTAL SCORE : {total_comp_score}")
 
     if total_comp_score >= WIN_SCORE:
         print("COMPUTER WINS!")
