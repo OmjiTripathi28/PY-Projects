2b73e4c741648e54710802c02f35083856c404c5
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-02-11 23:23:57 +0530

    added play function to it

--- EASY/rock_paper_scissors_lizard_spock.py
+++ EASY/rock_paper_scissors_lizard_spock.py
@@ -11,3 +11,27 @@ win_rules = {
     "spock": ["rock", "scissors"]
 }
 
+def play():
+    user = input("Enter rock / paper / scissors / lizard / spock: ").lower()
+
+    if user not in choices:
+        print("Invalid choice!")
+        return
+
+    computer = random.choice(choices)
+    print(f"Computer chose: {computer}")
+
+    if user == computer:
+        print("It's a tie!")
+    elif computer in win_rules[user]:
+        print("You win!")
+    else:
+        print("Computer wins!")
+
+while True:
+    play()
+    again = input("Play again? (y/n): ").lower()
+    if again != "y":
+        break
+
+print("Thanks for playing!")
