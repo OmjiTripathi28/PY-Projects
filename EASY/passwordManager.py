ba8d0576e9fc875ff63cf694090ee5fa62d3fd9d
OmjiTripathi28 <omji.tripathi.28@gmail.com>
2026-02-12 23:43:32 +0530

    made a new small project

---
+++ EASY/passwordManager.py
@@ -0,0 +1,12 @@
+def mode():
+    pass
+
+main_password = input ("Enter the main password to have access or to store passwords : ")
+with open ("passwords.txt", "r") as f :
+    text = f.read()
+    for main_password in text:
+        main_pass == main_pas
+
+    else:
+        print("invalid passowrd try again!")
+        
